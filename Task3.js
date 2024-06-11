1f59ec4362c865507cceccc263e697d958f62bd0
Tat <151627082+SuesmairT@users.noreply.github.com>
2024-06-11 14:33:23 +0200

    Update Task3.js

--- Task3.js
+++ Task3.js
@@ -1,6 +1,6 @@
 //Вывести ко-во видео-роликов с тегом mega и продолжительностью от 10 мин
 db.users.countDocuments(
     { tags: 'mega',
-	    duration_secs:  {$lte: 10 * 60 }
+	    duration_secs:  {$gte: 10 * 60 }
     }
 )
