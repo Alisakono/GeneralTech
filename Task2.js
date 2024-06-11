0680908a949a2e1e1c12d2557763d1987ede62e9
Trinity <alles.aug.2000@gmail.com>
2024-06-11 14:19:53 +0200

    update

---
+++ Task2.js
@@ -0,0 +1,11 @@
+db.videos.updateMany
+(   { duration_secs: {
+            $gte: 5 * 60,
+            $lte: 60 * 60
+
+    }
+       },
+    { $addToSet: {
+            tags: { $each: ['alpha', 'beta'] }
+        }}
+)
