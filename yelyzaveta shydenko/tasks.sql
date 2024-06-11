4f7e824664b6e343caa260bbaeb959ce652aabac
Yelyzaveta Shydenko <lizawinsdale@gmail.com>
2024-06-11 14:18:44 +0200

    add seccond  task

--- yelyzaveta shydenko/tasks.sql
+++ yelyzaveta shydenko/tasks.sql
@@ -2,3 +2,9 @@ db.users.find(
 {is_blocked: null, balance: {$lte: 1000000}},
 {fullname: 1, _id: 0}
 )
+
+db.videos.updateMany( 
+{ duration_secs: { $lte: 5 * 60, $gte: 60 * 60 } }, 
+{ $addToSet: { tags: { $each: ['alpha', 'beta'] } } }
+)
+
